7d879a443f0a83ac510ea791810d8e5610ecf4eb
predrvg <predragradovanovich@gmail.com>
2026-05-18 13:17:59 +0200

    Implementirano ucitavanje iz CSV fajla i reject log upis

--- Client/Program.cs
+++ Client/Program.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
 using System.Text;
+using System.IO;
+using System.Globalization;
 
 namespace Client
 {
@@ -15,6 +17,101 @@ namespace Client
             new ChannelFactory<IWeatherService>("WeatherService");
 
             IWeatherService service = factory.CreateChannel();
+
+            string csvPath = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "Data",
+                "weather.csv");
+
+            if (!File.Exists(csvPath))
+            {
+                Console.WriteLine("CSV file was not found.");
+                return;
+            }
+
+            string rejectLogPath = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "rejects.log");
+
+            if (File.Exists(rejectLogPath))
+            {
+                File.Delete(rejectLogPath);
+            }
+
+            List<WeatherSample> samples = new List<WeatherSample>();
+
+            using (StreamReader reader = new StreamReader(csvPath))
+            {
+                string headerLine = reader.ReadLine();
+
+                string line;
+
+                int rowCount = 0;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(',');
+
+                    if (values.Length < 10)
+                    {
+                        File.AppendAllText(
+                            rejectLogPath,
+                            $"Invalid column count: {line}{Environment.NewLine}");
+
+                        continue;
+                    }
+
+                    try
+                    {
+                        WeatherSample sample = new WeatherSample
+                        {
+                            Date = DateTime.ParseExact(
+                            values[0],
+                            "yyyy-MM-dd HH:mm:ss",
+                            CultureInfo.InvariantCulture),
+
+                            Pressure = double.Parse(
+                            values[1],
+                            CultureInfo.InvariantCulture),
+
+                            T = double.Parse(
+                            values[2],
+                            CultureInfo.InvariantCulture),
+
+                            Tpot = double.Parse(
+                            values[3],
+                            CultureInfo.InvariantCulture),
+
+                            Tdew = double.Parse(
+                            values[4],
+                            CultureInfo.InvariantCulture),
+
+                            Rh = double.Parse(
+                            values[5],
+                            CultureInfo.InvariantCulture),
+
+                            Sh = double.Parse(
+                            values[9],
+                            CultureInfo.InvariantCulture)
+                        };
+
+                        samples.Add(sample);
+
+                        rowCount++;
+
+                        if (rowCount >= 100)
+                        {
+                            break;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        File.AppendAllText(
+                            rejectLogPath,
+                            $"Parsing error: {line} | Error: {ex.Message}{Environment.NewLine}");
+                    }
+                }
+            }
 
             var metadata = new SessionMetadata
             {
@@ -30,31 +127,6 @@ namespace Client
             ServiceResponse startResponse = service.StartSession(metadata);
 
             Console.WriteLine($"{startResponse.Ack} - {startResponse.Status} - {startResponse.Message}");
-
-            var samples = new List<WeatherSample>
-            {
-                new WeatherSample
-                {
-                    T = 20,
-                    Pressure = 1013,
-                    Tpot = 19,
-                    Tdew = 10,
-                    Rh = 50,
-                    Sh = 5,
-                    Date = DateTime.Now
-                },
-
-                new WeatherSample
-                {
-                    T = 21,
-                    Pressure = 1012,
-                    Tpot = 20,
-                    Tdew = 11,
-                    Rh = 52,
-                    Sh = 6,
-                    Date = DateTime.Now
-                }
-            };
 
             for (int i = 0; i < samples.Count; i++)
             {
